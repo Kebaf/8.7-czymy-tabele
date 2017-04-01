abe4f1966d2fb66ec73612b20d243718c91329c2
ENTER <ENTER>
2017-04-01 23:42:02 +0200

    poprawione

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
 var femaleNames = ['Asia', 'Kasia', 'Ola', 'Jola'],
 maleNames = ['Piotrek', 'Marek', 'Arek', 'Jarek'],
-allNames = [],
-allNames = allNames.concat(femaleNames,maleNames);
-var newName = 'Marian';
-if (allNames.indexOf(newName) === -1) {
+allNames = femaleNames.concat(maleNames);
+newName = 'Marian';
+if (allNames.indexOf(newName) === -1) /* w zasadzie ta reguła co robi? (allNames.indexOf(newName) === -1 bo nie do końca rozumiem dodałem bo w zadaniu chcieli :)*/{
 	var x = allNames.push('Marian')
 }
 console.log(allNames);
